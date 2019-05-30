f5535b859e7f1931a5cd9507c8e08ae37848649a
0baldThePedro <pedro.nunes.loureiro@gmail.com>
2019-05-30 10:30:48 +0100

    changes

--- bin/run.rb
+++ bin/run.rb
@@ -2,9 +2,7 @@ require_relative '../config/environment'
 require_relative '../db/seeds.rb'
 
 
-
 Terminal.run
-
 
 Pry.start
 #
--- lib/cli/main.rb
+++ lib/cli/main.rb
@@ -1,26 +1,39 @@
 class Terminal
 
-
   def initialize
     @prompt = TTY::Prompt.new
-    @user_access = ""
   end
-
   def greeting
-    puts "Welcome"
+    puts "|.........................|"
+    puts "|.........................|"
+    puts "|.........................|"
+    puts "|Welcome to the Pet World!|"
+    puts "|.........................|"
+    puts "|.........................|"
+    puts "|.........................|"
     @prompt.yes?('Are you a new user?')
   end
 
   def signup
+   password = ''
+   confirm_password = '' 
+  
    first_name = @prompt.ask('What is your first name?')
    last_name = @prompt.ask('What is your last name?')
    post_code = @prompt.ask('What is your postcode?')
    age = @prompt.ask('How old are you?', convert: :int)
-   password = @prompt.mask('Enter a password')
-   @user = User.create(first_name: first_name, last_name: last_name, post_code: post_code,password: password, age: age)
-   puts @user
-   puts "Account Created!"
-   false
+
+   loop do
+    password = @prompt.mask('Enter a password')
+    confirm_password = @prompt.mask("Please confirm your password!")
+    if password != confirm_password
+      puts "Please, make sure your password and confirmation password are matching."
+    end
+    break if password == confirm_password
+  end
+  @user = User.create(first_name: first_name, last_name: last_name, post_code: post_code,password: password, age: age)
+  puts @user
+  puts "Account successfully created!"
   end
 
 
@@ -29,7 +42,6 @@ class Terminal
     password = @prompt.mask("Please enter your password!")
     @user_access = User.find_by(first_name: first_name, password: password)
     !@user_access.nil?
-
   end
 
   def clean
@@ -44,18 +56,21 @@ class Terminal
 
 
   def show_menu
-    @options = ["Show me all the pets", "Make a donation", "Adopt a pet", "Show me pets nearby"]
+    "Welcome #{@user_access.first_name}"
+    @options = ["Show me all the pets", "Make a donation", "Adopt a pet", "Show me pets nearby", "Exit"]
     @option = @prompt.select("What would you like to do?", @options)
-    if @option == @options[0]
-      show_pets
-    elsif @option == @options[1]
-      make_donation
-    elsif @option == @options[2]
-      adopt_pet
-    elsif @option == @options[3]
-      adopt_pet_for_real      
+        if @option == @options[0]
+          show_pets
+        elsif @option == @options[1]
+          make_donation
+        elsif @option == @options[2]
+          adopt_pet
+        elsif @option == @options[3]
+          adopt_pet_for_real
+        elsif @option == @options[4]
+          false
+        end
     end
-  end
 
   
 
@@ -82,14 +97,13 @@ class Terminal
 
   def self.run
     cli = new
+    cli.clean
     loop do
       break if (cli.greeting ? cli.signup : cli.login)
     end
-      cli.show_menu
-
-    
-
-    
+    loop do
+      break if cli.show_menu == false
+    end
   end
 
   def adopt_pet
